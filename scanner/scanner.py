a8f3ada54f4df6177e4e575af6543a0bd972465c
Monastyr <60261397+Monastyr@users.noreply.github.com>
2021-03-30 14:11:47 +0200

    Update scanner.py

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -19,7 +19,7 @@ DBLINK = 'mongodb://localhost:27018/'
 #DBLINK = 'mongodb://autoenum_mongodb:27018/'
 TARGETFILE = "target.txt"
 LOG_FORMAT = "%(name)s %(asctime)s - %(message)s"
-FILENAME = "/var/lib/docker/volumes/docker_log-volume/_data/Scanner.log"
+FILENAME = "/var/lib/docker/volumes/files_log-volume/_data/Scanner.log"
 
 def exclude_self():
         host_ip = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
